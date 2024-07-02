1ca7277a70fe77ad4bb35732b78a458d3125cb21
luis2708a <luis270899@gmail.com>
2024-07-02 01:26:44 -0600

    Update Guardar.js

--- Js/Guardar.js
+++ Js/Guardar.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+/*document.addEventListener('DOMContentLoaded', () => {
     const savedNombreEmp = localStorage.getItem('NombreEmp');
     if (savedNombreEmp) {
         document.getElementById('NombreEmp').value = savedNombreEmp;
@@ -9,5 +9,20 @@ document.addEventListener('DOMContentLoaded', () => {
         const NombreEmp = document.getElementById('NombreEmp').value;
         localStorage.setItem('NombreEmp', NombreEmp);
         alert('Información guardada!');
+    });
+});*/
+
+//Etapa 0
+document.addEventListener('DOMContentLoaded',()=>{
+    const guardarEtapa0 = localStorage.getItem('Etapa0');
+    if (guardarEtapa0) {
+        document.getElementById('Etapa0').value = guardarEtapa0;
+    }
+
+    document.getElementById('inputForm').addEventListener('submit', function(event){
+        event.preventDefault();
+        const Etapa0 = document.getElementById('Etapa0').value;
+        localStorage.setItem('Etapa0', Etapa0);
+        alert('Información guardada');
     });
 });
