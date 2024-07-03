a561ed802ceef751939954bc730a7ee57555c223
Tavo26pum <foctavio494@gmail.com>
2024-07-03 04:08:27 -0600

    as
    aaa

--- Js/Pintar.js
+++ Js/Pintar.js
@@ -6,6 +6,7 @@ function llamar_datos(){
     document.getElementById('Fecha').textContent = llamar_r[0].area;
     //document.getElementById('Responsable').textContent = llamar_r[1].area;
     document.getElementById('CostoActividad').textContent = llamar_r[2].area;
+    document.getElementById('TiempoPropuesta').textContent = llamar_r[3].area;
 
 
 }
@@ -14,6 +15,7 @@ function llamar_datos2(){
     const traer_datos = localStorage.getItem('datos');
     const llamar_r2 = JSON.parse(traer_datos);
     document.getElementById('Responsable').textContent = llamar_r2.nombre_empresa;
+    document.getElementById('TiempoPropuesta').textContent = llamar_r2.nombre_empresa;
 }
 
 function plangeneral(){
