eeacb76a976193b7e7890f19cd7c1c5024e84e71
luis2708a <luis270899@gmail.com>
2024-07-02 23:54:19 -0600

    guardar

--- Js/Guardar.js
+++ Js/Guardar.js
@@ -9,6 +9,7 @@ document.getElementById('inputForm').addEventListener('submit', function (event)
     /*const NombreEmp = document.getElementById('NombreEmp').value;
     localStorage.setItem('NombreEmp', NombreEmp);*/
     guardar_propuesta();
+    guardar_bench();
     //alert('Información guardada!');
 });
 /*});*/
@@ -60,11 +61,120 @@ function guardar_propuesta() {
         responsable_tec: responsable_tec,
         responsable_fi: responsable_fi
     };
-    alert('Información guardada!');
-    console.log(datos);
+    alert('Información guardada');
 
     localStorage.setItem('datos', JSON.stringify(datos))
-    console.log("si se guardo la info")
+    //console.log("si se guardo la info")
+}
+
+function guardar_bench(){
+    const fecha_bench = document.getElementById('FechaEtapa0Benchmarking').value;
+    const costo_bench = document.getElementById('CBenchmarking').value;
+    const tiempo_bench = document.getElementById('TBenchmarking').value;
+    const molm = document.getElementById('MolM').value;
+    const mopo = document.getElementById('Mopo').value;
+    const maten = document.getElementById('Maten').value;
+    const mcal = document.getElementById('Mcal').value;
+    const mweb = document.getElementById('Mweb').value;
+
+    const datos2 = {
+        fecha_bench: fecha_bench,
+        costo_bench: costo_bench,
+        tiempo_bench: tiempo_bench,
+        molm: molm,
+        mopo: mopo,
+        maten: maten,
+        mcal: mcal,
+        mweb: mweb
+    };
+    alert('Información guardada');
+
+    localStorage.setItem('datos2', JSON.stringify(datos2))
+}
+
+function guardar_demanda(){
+    const fecha_demanda = document.getElementById('FechaEtapa0Determinacion').value;
+    const costo_demanda = document.getElementById('CDeterminacion').value;
+    const tiempo_demanda = document.getElementById('TDeterminacion').value;
+    const deteccionmeta = document.getElementById('Deteccionmeta').value;
+    const satisfacionmeta = document.getElementById('Satisfacionmeta').value;
+    const demanmeta = document.getElementById('Demanmeta').value;
+    const deteccionobj = document.getElementById('Deteccionobj').value;
+    const satisfacionobj = document.getElementById('Satisfacionobj').value;
+    const demanobj = document.getElementById('Demanobj').value;
+    const deteccionpot = document.getElementById('Deteccionpot').value;
+    const satisfacionpot = document.getElementById('Satisfacionpot').value;
+    const demanpot = document.getElementById('Demanpot').value;
+
+    const datos3 = {
+        fecha_demanda: fecha_demanda,
+        costo_demanda: costo_demanda,
+        tiempo_demanda: tiempo_demanda,
+        deteccionmeta: deteccionmeta,
+        satisfacionmeta: satisfacionmeta,
+        demanmeta: demanmeta,
+        deteccionobj: deteccionobj,
+        satisfacionobj: satisfacionobj,
+        demanobj: demanobj,
+        deteccionpot: deteccionpot,
+        satisfacionpot: satisfacionpot,
+        demanpot: demanpot
+    };
+    alert('Información guardada');
+
+    localStorage.setItem('datos3', JSON.stringify(datos3))
+}
+
+function guardar_identificacion(){
+    const fecha_identi = document.getElementById('FechaEtapa0Mercado').value;
+    const costo_identi = document.getElementById('CIdentificacion').value;
+    const tiempo_identi = document.getElementById('TIdentificacion').value;
+    const mercadometa = document.getElementById('MercadoMetaI').value;
+    const mercadoobj = document.getElementById('MercadoObjetivoI').value;
+    const mercadopoten = document.getElementById('MercadoPotencialI').value;
+    const costo_estra = document.getElementById('CEstratificación').value;
+    const tiempo_estra = document.getElementById('TEstratificación').value;
+    const mercadometaE = document.getElementById('MercadoMetaE').value;
+    const mercadoobjE = document.getElementById('MercadoObjetivoE').value;
+    const mercadopotenE = document.getElementById('MercadoPotencialE').value;
+
+    const datos4 = {
+        fecha_identi: fecha_identi,
+        costo_identi: costo_identi,
+        tiempo_identi: tiempo_identi,
+        mercadometa: mercadometa,
+        mercadoobj: mercadoobj,
+        mercadopoten: mercadopoten,
+        costo_estra: costo_estra,
+        tiempo_estra: tiempo_estra,
+        mercadometaE: mercadometaE,
+        mercadoobjE: mercadoobjE,
+        mercadopotenE: mercadopotenE
+    };
+    alert('Información guardada')
+
+    localStorage.setItem('datos4', JSON.stringify(datos4))
+}
+
+function guardada_comercial(){
+    const fecha_comer = document.getElementById('FechaEtapa0Comercial').value;
+    const costo_comer = document.getElementById('CPlanComercial').value;
+    const actividad_comer = document.getElementById('ActividadPC').value;
+    const tiempo_comer = document.getElementById('TiempoPC').value;
+    const cantidades_comer = document.getElementById('CantidadesPC').value;
+    const precio_comer = document.getElementById('PrecioPC').value;
+
+    const datos5 = {
+        fecha_comer: fecha_comer,
+        costo_comer: costo_comer,
+        actividad_comer: actividad_comer,
+        tiempo_comer: tiempo_comer,
+        cantidades_comer: cantidades_comer,
+        precio_comer: precio_comer
+    };
+    alert('Información guardada')
+
+    localStorage.setItem('datos5', JSON.stringify(datos5))
 }
 
 /*function mostrar(){
--- Js/Pintar.js
+++ Js/Pintar.js
@@ -2,14 +2,13 @@
 
 function llamar_datos(){
     const traer_datos = localStorage.getItem('datos');
-    const llamar_fecha = JSON.parse(traer_datos);
-    document.getElementById('Fecha').textContent = llamar_fecha.fecha_inicio;
-    document.getElementById('Responsable').textContent = llamar_fecha.nombre_empresa;
+    const llamar_r = JSON.parse(traer_datos);
+    document.getElementById('Fecha').textContent = llamar_r.fecha_inicio;
+    document.getElementById('Responsable').textContent = llamar_r.nombre_empresa;
 }
 
 function llamar_datos2(){
     const traer_datos = localStorage.getItem('datos');
-    const llamar_r = JSON.parse(traer_datos);
-    document.getElementById('Responsable').textContent = llamar_r.nombre_empresa;
-    console.log(llamar_r);
+    const llamar_r2 = JSON.parse(traer_datos);
+    document.getElementById('Responsable').textContent = llamar_r2.nombre_empresa;
 }
